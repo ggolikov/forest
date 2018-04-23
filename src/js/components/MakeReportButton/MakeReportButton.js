61943f9645a98133ff3f0dcde3c23dbf0ad67392
ggolikov <gr.golikov@gmail.com>
2018-04-23 13:16:39 +0300

    Update server preview template

--- src/js/components/MakeReportButton/MakeReportButton.js
+++ src/js/components/MakeReportButton/MakeReportButton.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { BeatLoader } from 'react-spinners';
 import Button from '../Button';
-import { collectParams, makeReport } from '../../helpers';
+import { collectParams, makeReport, addStatusColumn, updateObjects } from '../../helpers';
+import { TRUE_STATUS_COLUMN_NAME } from '../../constants';
+import { setAttributesList, updateFeatures } from '../../AC';
 import './index.sass';
 
 class MakeReportButton extends Component {
@@ -11,6 +13,51 @@ class MakeReportButton extends Component {
         this.state = {
             isLoading: false,
             error: false
+        }
+    }
+
+    changeFeaturesStatus = (features) => {
+        const state = window.store.getState();
+
+        const { layerId, attributesList } = state;
+        const selectedFeatures = features.filter(item => item.selected);
+        const objectsToUpdate = selectedFeatures
+            .map(item => {
+                return {
+                    action: "update",
+                    id: item.id,
+                    properties: {
+                        [TRUE_STATUS_COLUMN_NAME]: 2
+                    }
+                }
+            });
+
+        const hasStatusAttribute = attributesList.find(v => v === TRUE_STATUS_COLUMN_NAME);
+
+        if (hasStatusAttribute) {
+            updateObjects(layerId, objectsToUpdate)
+                .then(res => {
+                    const updatedFeatures = selectedFeatures.map(feature => {
+                        feature.status = 2;
+                        return feature;
+                    });
+                    window.store.dispatch(updateFeatures(updatedFeatures));
+                });
+        } else {
+            addStatusColumn(layerId)
+                .then(res => {
+                    const updatedAttributesList = attributesList.slice();
+                    updatedAttributesList.push(TRUE_STATUS_COLUMN_NAME);
+                    window.store.dispatch(setAttributesList(updatedAttributesList));
+                    updateObjects(layerId, objectsToUpdate)
+                        .then(res => {
+                            const updatedFeatures = selectedFeatures.map(feature => {
+                                feature.status = 2;
+                                return feature;
+                            });
+                            window.store.dispatch(updateFeatures(updatedFeatures));
+                        });
+                });
         }
     }
 
@@ -25,6 +72,7 @@ class MakeReportButton extends Component {
         console.log(reportParams);
         makeReport(reportParams)
             .then(res => {
+                this.changeFeaturesStatus(features);
                 this.setState({
                     isLoading: false,
                     error: false
